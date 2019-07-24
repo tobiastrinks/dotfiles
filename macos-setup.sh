c90228eefd934f9ca9c77a2e9d340c5bad1ef79d
Tobias Trinks <tobias@Tobiass-MacBook-Pro.local>
2019-07-24 23:34:52 +0200

    Add macos setup script

---
+++ macos-setup.sh
@@ -0,0 +1,58 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew cask install iterm2
+brew cask install sublime-text
+brew cask install google-chrome
+brew cask install dropbox
+brew cask install docker
+
+if (! docker stats --no-stream)
+then
+    # start docker if not running
+    open /Applications/Docker.app
+fi
+
+brew cask install spotify
+brew cask install slack
+
+brew cask install webstorm
+brew cask install datagrip
+brew cask install intellij-idea
+
+brew install tmux
+echo -n '
+unbind C-b
+set-option -g prefix C-a
+bind-key C-a send-prefix
+set -g mouse on
+' > ~/.tmux.conf
+bind r source-file ~/.tmux.conf
+
+brew install node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+touch ~/.bash_profile
+touch ~/.bash_aliases
+
+echo -n '
+if [ -f ~/.bash_aliases ]; then
+  source ~/.bash_aliases
+fi
+' >> ~/.bash_profile
+
+echo -n '
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+' >> ~/.bash_profile
+
+echo -n '
+alias ll="ls -la"
+alias c="clear"
+alias work="cd ~/workspace"
+alias he="cd ~/workspace/hotel-edison"
+alias keeet="cd ~/workspace/keeet"
+' >> ~/.bash_aliases
+
+source ~/.bash_profile
